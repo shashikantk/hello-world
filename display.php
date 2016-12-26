1f2a2d998de572e82d7984fff6f61f460abe172a
shashikant <shashikant.kuswaha@sts.in>
2016-12-26 14:34:38 +0530

    Create display.php

---
+++ display.php
@@ -0,0 +1,24 @@
+//file to display text on click
+<?php
+  $data=isset($_REQUEST['text'])?$_REQUEST['text']:'';
+  if($data!=''){
+    echo $data;
+    
+  }
+?>
+
+<html>
+<head>
+<title>
+Display Text !!!
+</title>
+</head>
+<body>
+  <form action="#" method="get" >   
+    <label>Enter Text here</label>
+    <input type="text" name="text">
+    <input type="submit" value="click here">
+  </form>
+</body>
+
+</html>
